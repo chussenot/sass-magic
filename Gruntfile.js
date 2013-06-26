44893372c49fff201c1ce371cad9c014d926c875
Clément Hussenot <chussenot@gmail.com>
2013-06-26 22:21:55 +0200

    Use grunt
    few tasks to help sass/compass implementation
    add a testing page

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+/*globals module */
+module.exports = function (grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+    meta: {
+      banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %> */'
+    },
+
+    concat: {
+      build: {
+        src: ['partials/*.scss'],
+        dest: 'magic.scss'
+      }
+    },
+
+    sass: {
+      css: {
+        options: {
+          unixNewlines: true,
+          style: 'expanded'
+        },
+        files: {
+          'tests/css/main.css': 'tests/scss/main.scss'
+        }
+      }
+    },
+
+    release: {
+      options: {
+        file: 'package.json',
+        push: false,
+        npm: false,
+        tagName: 'v<%= version %>',
+        commitMessage: 'Release for <%= version %>'
+      }
+    }
+  });
+
+  // Load some stuff
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-release');
+
+  // Default task
+  grunt.registerTask('default', ['concat', 'sass']);
+
+};
